103c2d039686272fa22e13aef25e2926df9a87ac
Vadim Molchanov <fright.end@mail.ru>
2021-07-16 12:00:23 +0300

    DrupalFirstForm #2

--- modules/src/Form/SimpleForm.php
+++ modules/src/Form/SimpleForm.php
@@ -32,17 +32,17 @@ class SimpleForm extends FormBase {
 
     $config = \Drupal::config('helloworld.simple_form.settings');
 
-    $form['phone_number'] = array(
-      '#type' => 'tel',
-
-      '#title' => $this->t('Your phone number'),
-      '#default_value' => $config->get('phone_number')
-    );
+//    $form['phone_number'] = array(
+//      '#type' => 'tel',
+//      '#title' => $this->t('Your phone number'),
+//      '#default_value' => $config->get('phone_number')
+//    );
 
     $form['name'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Your name')
     );
+
 
     $form['last_name'] = array(
       '#type' => 'textfield',
@@ -55,9 +55,8 @@ class SimpleForm extends FormBase {
     );
 
     $form['messages'] = array(
-      'type' => 'textfield',
-      'size' => 'long',
-      'not null' => FALSE,
+      '#type' => 'textfield',
+      '#size' => 'long',
       '#title' => $this->t('Your message')
     );
 
@@ -81,6 +80,7 @@ class SimpleForm extends FormBase {
   /**
    *
    * {@inheritdoc}
+   * @throws \Exception
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
 //    if (strlen($form_state->getValue('name')) < 5) {
@@ -90,7 +90,6 @@ class SimpleForm extends FormBase {
     {
       drupal_set_message($this->t('Thank you @name, your Email is @email', array(
         '@name' => $form_state->getValue('name'),
-        '@number' => $form_state->getValue('phone_number'),
         '@email' => $form_state->getValue('email_')
 
       )));
@@ -126,14 +125,27 @@ class SimpleForm extends FormBase {
   /**
    *
    * {@inheritdoc}
+   * @throws \Exception
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+//    $dk = db_insert('exampleform') -> fields(array(
+//      'name' => $form_state['values'] ['fname'],
+//      'last_name' => $form_state['values'] ['lname'],
+//      'Subject' => $form_state['values']['subjectbd'],
+//      'messages' => $form_state['values']['messagesbd'],
+//      'phone_number' => $form_state['values']['phone_number'],
+//      'email_' => $form_state['values']['	email'],
+//
+//    ))
+//      -> execute();
+//    dsm($dk);
 
-    drupal_set_message($this->t('Thank you @name, your Email is @email', array(
-      '@name' => $form_state->getValue('name'),
-      '@number' => $form_state->getValue('phone_number'),
-      '@email' => $form_state->getValue('email_')
-    )));
+//    drupal_set_message($this->t('Thank you @name, your Email is @email', array(
+//      '@name' => $form_state->getValue('name'),
+//      '@email' => $form_state->getValue('email_')
+//    )));
+
+
   }
 
 }
